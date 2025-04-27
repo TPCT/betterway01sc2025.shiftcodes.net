a55f73ef79ba647f6e0d69d006d590a64af74a80
tpct <eslamjackie@outlook.com>
2025-04-27 10:53:27 +0300

    adding Fifth Agency From Client Side            'FlowStatus' => $FlowStatus,

--- app/Helpers/GeneralHelper.php
+++ app/Helpers/GeneralHelper.php
@@ -792,7 +792,7 @@ function generateBatchNumber($AgencyPlanNetwork){
 }
 
 function createNode($Client, $ParentPlanNetwork, $IDPlanProduct, $PlanNetworkExpireDate, $Position, $Index, $Points, $AgencyFor = null){
-    $AgencyClient = createAgencyClient($Client, $Position, $Index, null, null, $AgencyFor);
+    $AgencyClient = createAgencyClient($Client, $Position, $Index, $AgencyFor, null, $AgencyFor);
     $AgencyClient->{"Client" . Str::ucfirst(Str::lower($Position)) . "Points"} += $Points;
     $AgencyClient->ClientTotalPoints += $Points;
     $AgencyClient->save();
