5760eea7c734c3420756b9ca4f3e8fd763b7a1f5
tpct <eslamjackie@outlook.com>
2025-04-30 14:56:40 +0300

    fixing all over the app adjust ledger

--- app/Http/Controllers/Admin/Client/ClientController.php
+++ app/Http/Controllers/Admin/Client/ClientController.php
@@ -563,6 +563,9 @@ class ClientController extends Controller
             return RespondWithBadRequest(1);
         }
 
+        var_dump("hello world");
+        exit;
+
         $Client = Client::where(['IDClient' => $request->IDClient, 'ClientStatus' => 'ACTIVE'])->first();
         if (!$Client) {
             return RespondWithBadRequest(10);
@@ -584,8 +587,6 @@ class ClientController extends Controller
             return RespondWithBadRequest(25);
         }
 
-        var_dump("hello world");
-        exit;
         if ($ParentClient) {
             $ParentPlanNetwork = PlanNetwork::where("IDClient", $ParentClient->IDClient)->first();
             $IDParentClient = $IDUpline;
